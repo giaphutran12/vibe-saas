4ca1c929e11b5c3dece34677841f6cbc5935d4e4
Edward Tran <giaphutran012@gmail.com>
2025-07-27 02:21:22 -0700

    07: agent tools

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const assistantMessages = result.output.filter(
+    (message) => message.role === "assistant"
+  );
+  const lastMessage = assistantMessages[assistantMessages.length - 1] as
+    | TextMessage
+    | undefined;
+  return lastMessage?.content
+    ? typeof lastMessage.content === "string"
+      ? lastMessage.content
+      : lastMessage.content.map((c) => c.text).join("")
+    : undefined;
 }
