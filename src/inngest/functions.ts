714f82eb783d3e274588cc9f2d99ed467b35f1dc
Edward Tran <giaphutran012@gmail.com>
2025-07-26 20:44:15 -0700

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { inngest } from "./client";
 import { grok, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld2 = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-next-js-test5");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code agent",
       system:
@@ -16,6 +22,13 @@ export const helloWorld2 = inngest.createFunction(
       `Code the following snippet: ${event.data.value}`
     );
     console.log(output);
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
