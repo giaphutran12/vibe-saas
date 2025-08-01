502666f44172bebf0d36f90840d7c5ee4bb37dc1
Edward Tran <giaphutran012@gmail.com>
2025-07-31 19:10:17 -0700

    19.1.1 remove @typescript unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,6 @@ const eslintConfig = [
     ignores: ["**/generated/*"],
     rules: {
       "no-unused-vars": "warn",
-      "@typescript/no-unused-vars": "warn", // also add this if you're using typescript
     },
   },
 ];
